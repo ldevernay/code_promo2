d39304ff0b9f1e2570516e73cd8ada78567280ae
Simon Darmandieu <simon.darmandieu@gmail.com>
2018-07-04 22:23:48 +0200

    ternary operators in comments

--- Randoris/006 - ChiffreDeCesar/Version unicode et TDD/app.js
+++ Randoris/006 - ChiffreDeCesar/Version unicode et TDD/app.js
@@ -12,3 +12,18 @@ function caesar (str,cypher) {
 }
 
 module.exports = caesar ;
+
+/* Version avec Ternary operator à la place des if else
+
+function caesar (str,cypher) {
+	return cypher<1 || cypher>25 
+	? "cypher between 1 and 25 only" 
+	: str.toUpperCase().split('').map(function(x){
+		return x.charCodeAt(0)>=65 && x.charCodeAt(0)<=90
+		? String.fromCharCode((x.charCodeAt(0)+cypher-65)%26+65) : x
+	}).join('');
+}
+
+module.exports = caesar ;
+
+*/
