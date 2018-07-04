20d055a2e347bfe1ba8171b3a8e1ec33f250ff03
Simon Darmandieu <simon.darmandieu@gmail.com>
2018-07-04 22:56:18 +0200

    es6

--- Randoris/006 - ChiffreDeCesar/Version unicode et TDD/test.js
+++ Randoris/006 - ChiffreDeCesar/Version unicode et TDD/test.js
@@ -1,34 +1,34 @@
-var expect = require('chai').expect;
-var caesar = require('./app.js');
+const expect = require('chai').expect;
+const caesar = require('./app.js');
 
 describe('caesar()' ,() => {
 	it('should return a string' ,() => {
-		var str = typeof "test";
-		var mystr = typeof caesar("test",5);
+		let str = typeof "test";
+		let mystr = typeof caesar("test",5);
 		expect(str).to.be.equal(mystr);
 	});
 
 	it('should return "NSTQQCP OP NPDLC" ' ,()=>{
-		var str = "NSTQQCP OP NPDLC";
-		var mystr = caesar("chiffre de cesar",11);
+		let str = "NSTQQCP OP NPDLC";
+		let mystr = caesar("chiffre de cesar",11);
 		expect(str).to.be.equal(mystr);
 	});
 
 	it('should return "EFGHIJKLMNOPQRSTUVWXYZABCD" ' , ()=>{
-		var str = "EFGHIJKLMNOPQRSTUVWXYZABCD";
-		var mystr = caesar("ABCDEFGHIJKLMNOPQRSTUVWXYZ",4);
+		let str = "EFGHIJKLMNOPQRSTUVWXYZABCD";
+		let mystr = caesar("ABCDEFGHIJKLMNOPQRSTUVWXYZ",4);
 		expect(str).to.be.equal(mystr);
 	});
 
 	it('should return "Z" ' ,()=>{
-		var str = "Z";
-		var mystr = caesar("v",4);
+		let str = "Z";
+		let mystr = caesar("v",4);
 		expect(str).to.be.equal(mystr);
 	});
 
 	it('should say "cypher between 1 and 25 only" ' ,()=>{
-		var str = "cypher between 1 and 25 only";
-		var mystr = caesar("test",-6);
+		let str = "cypher between 1 and 25 only";
+		let mystr = caesar("test",-6);
 		expect(str).to.be.equal(mystr);
 	});
 });
